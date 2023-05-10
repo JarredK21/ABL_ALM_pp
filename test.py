eb71686e76b86ca407d58d66d168175e1fab96f9
JarredK21 <jarred.kenworthy@strath.ac.uk>
2023-05-10 15:24:23 -0600

    plotting script

---
+++ test.py
@@ -0,0 +1,49 @@
+import pyFAST.input_output as io
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from scipy import interpolate
+from scipy.signal import butter,filtfilt
+from scipy.stats import pearsonr
+import glob 
+from scipy import interpolate
+from netCDF4 import Dataset
+import pandas as pd
+from multiprocessing import Pool
+import time
+
+
+#openfast data
+df = io.fast_output_file.FASTOutputFile("../NREL_5MW_3.4.1/Steady_Rigid_blades/NREL_5MW_Main.out").toDataFrame()
+
+#sampling data
+sampling = glob.glob("../post_processing/sampling*")
+a = Dataset("./{}".format(sampling[0]))
+p_rotor = a.groups["p_sw1"]
+
+offsets = p_rotor.offsets
+
+Variables = ["Time_OF","Time_sample","Ux_{}".format(offsets[2]),"IA_{}".format(offsets[2]),"RtAeroFxh","RtAeroMxh","MR","Theta"]
+units = ["[s]","[s]", "[m/s]","[m^4/s]","[N]","[N-m]","[N-m]","[rads]"]
+
+
+dq = dict()
+
+time_OF = np.array(df["Time_[s]"])
+time_sample = np.array(a.variables["time"])
+time_sample = time_sample - time_sample[0]
+
+print(time_OF)
+print(time_sample)
+
+tstart = 50
+tend = 150
+tstart_OF_idx = np.searchsorted(time_OF,tstart)
+tend_OF_idx = np.searchsorted(time_OF,tend)
+tstart_sample_idx = np.searchsorted(time_sample,tstart)
+tend_sample_idx = np.searchsorted(time_sample,tend)
+
+dq["Time_OF"] = time_OF[tstart_OF_idx:tend_OF_idx]
+dq["Time_sample"] = time_sample[tstart_sample_idx:tend_sample_idx]
+
+print(dq["Time_OF"])
