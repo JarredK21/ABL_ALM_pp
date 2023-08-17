176e8cc7181338b6bd3a7eb9fdd69ce2d3887886
JarredK21 <jarred.kenworthy@strath.ac.uk>
2023-08-16 19:09:23 -0600

    print variables Dataset

--- Dataset.py
+++ Dataset.py
@@ -219,6 +219,8 @@ for offset in offsets:
         Variables.append(x)
         units.append(y)
 
+print(Variables)
+
 #sampling time
 Time_sample = np.array(a.variables["time"])
 Time_sample = Time_sample - Time_sample[0]
