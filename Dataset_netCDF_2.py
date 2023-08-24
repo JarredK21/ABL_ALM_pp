3e239b2d69deba2249db3bbaed0dc4240e437a9e
JarredK21 <jarred.kenworthy@strath.ac.uk>
2023-08-24 12:04:32 -0600

    debeug Dataset_2 3

--- Dataset_netCDF_2.py
+++ Dataset_netCDF_2.py
@@ -71,7 +71,7 @@ for iv in np.arange(0,len(Variables)):
         elif Variable == "Wind1VelX":
             RtAeroVxh[:] = signal; del signal
         elif Variable == "LSShftFya":
-            print(signal[0])
+            print(signal[0][0])
             LSShftFya[:] = signal; del signal
         elif Variable == "LSShftFza":
             LSShftFza[:] = signal; del signal
