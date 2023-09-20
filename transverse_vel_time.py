4f85b0899688509950ec43df3210cd3db7005bfd
JarredK21 <jarred.kenworthy@strath.ac.uk>
2023-09-20 15:42:27 -0600

    debug transverse vel time inc hub height func

--- transverse_vel_time.py
+++ transverse_vel_time.py
@@ -2,6 +2,7 @@ import pyFAST.input_output as io
 import matplotlib.pyplot as plt
 import numpy as np
 from netCDF4 import Dataset
+from scipy import interpolate
 from multiprocessing import Pool
 import time
 
@@ -10,15 +11,16 @@ def average_velocity(it):
     avg_velx_it = []
     for idx in height_idx:
         if plane == "t":
-            avg_velx_it.append(np.average(velocityx[it,idx:(idx+ys)]))
+            avg_velx_it.append(np.average(velocityx[it,idx:(idx+z)]))
         elif plane == "r":
-            hvelmag = np.add(np.multiply(velocityx[it,idx:(idx+ys)], np.cos(np.radians(29))) ,np.multiply(velocityy[it,idx:(idx+ys)], np.sin(np.radians(29))))
+            hvelmag = np.add(np.multiply(velocityx[it,idx:(idx+z)], np.cos(np.radians(29))) ,np.multiply(velocityy[it,idx:(idx+z)], np.sin(np.radians(29))))
             avg_velx_it.append(np.average(hvelmag))
     return avg_velx_it
 
 start_time = time.time()
 
-in_dir = "./"
+#in_dir = "./"
+in_dir = "../../NREL_5MW_MCBL_R_CRPM_2/post_processing/"
 out_dir = in_dir+"Quasi-stationarity/"
 
 heights = [10,40,90,500,1000,1200]
@@ -38,16 +40,47 @@ for offset,plane in zip(offsets,planes):
 
     p = a.groups["p_{}".format(plane)]
 
-    ys = p.ijk_dims[0]; zs = p.ijk_dims[1]
+    coordinates = np.array(p.variables["coordinates"])
 
-    coordinates = np.array(p.variables["coordiantes"])
-    #transform coordiates into rotor frame
-    z = coordinates[:,2]
+    if plane == "r":
+        y = p.ijk_dims[0] #no. data points
+        z = p.ijk_dims[1] #no. data points
 
-    if plot_average_y == True:
+        xo = coordinates[:,0]
+        yo = coordinates[:,1]
+        zo = coordinates[:,2]
+
+        rotor_coordinates = [2560,2560,90]
+
+        x_trans = xo - rotor_coordinates[0]
+        y_trans = yo - rotor_coordinates[1]
+
+        phi = np.radians(-29)
+        xs = np.subtract(x_trans*np.cos(phi), y_trans*np.sin(phi))
+        ys = np.add(y_trans*np.cos(phi), x_trans*np.sin(phi))
+        zs = zo - rotor_coordinates[2]
+
+        Y = np.linspace(round(np.min(ys),0), round(np.max(ys),0),y )
+        Z = np.linspace(round(np.min(zs),0), round(np.max(zs),0),z )
+
         height_idx = []
         for height in heights:
-            height_idx.append(np.searchsorted(z,height))
+            height_idx.append(np.searchsorted(zs,height))
+    else:
+        y = p.ijk_dims[0]; z = p.ijk_dims[1]
+        ys = coordinates[:,1]
+        zs = coordinates[:,2]
+        Y = np.linspace(round(np.min(ys),0), round(np.max(ys),0),y )
+        Z = np.linspace(round(np.min(zs),0), round(np.max(zs),0),z )
+
+        height_idx = []
+        for height in heights:
+            height_idx.append(np.searchsorted(zs,height))
+
+    for idx in height_idx:
+        print(coordinates[idx:(idx+z)])
+
+    if plot_average_y == True:
 
         velocityx = np.array(p.variables["velocityx"])
         if plane == "r":
@@ -80,7 +113,33 @@ for offset,plane in zip(offsets,planes):
 
     if plot_hub_height == True:
         height = 90
-        trans = 2555
-        height_idx = np.searchsorted(z,height)
-        coord_idx = np.searchsorted(coordinates[height_idx:(height_idx+ys),1],trans)
-        print(coordinates[coord_idx])
+        trans = 2560
+        hub_height_vel = []
+        for it in np.arange(0,len(time_idx)):
+
+            velx = np.reshape(velocityx[it],(z,y))
+            fx = interpolate.interp2d(Y,Z,velx,kind="linear")
+            Ux = fx(rotor_coordinates[1],rotor_coordinates[2])
+
+            if plane == "r":
+                vely = np.reshape(velocityy[it],(z,y))
+                fy = interpolate.interp2d(Y,Z,vely,kind="linear")
+                Uy = fy(rotor_coordinates[1],rotor_coordinates[2])
+                hub_height_vel.append(Ux*np.cos(np.radians(29))+Uy*np.sin(np.radians(29)))
+            else:
+                hub_height_vel.append(Ux)
+
+        fig = plt.figure(figsize=(14,8))
+        plt.plot(Time_sample,hub_height_vel)
+        plt.xlabel("Time [s]",fontsize=16)
+        if plane == "t":
+            plt.ylabel("Ux [m/s]",fontsize=16)
+            plt.title("Ux at hub height {}m from inlet".format(offset),fontsize=18)
+        elif plane == "r":
+            plt.ylabel("Ux' [m/s]",fontsize=16)
+            plt.title("Ux' at hub height {}m from tower centerline".format(offset),fontsize=18)
+        plt.legend(heights)
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig(out_dir+"avg_velocityx_{}_{}.png".format(plane,offset))
+        plt.close(fig)
