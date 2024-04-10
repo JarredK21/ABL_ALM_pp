3ee5da95a148e684b3909b28b6af8c1cb368d658
JarredK21 <jarred.kenworthy@strath.ac.uk>
2024-04-10 13:28:16 +0100

    debugging IHL

--- IHL_calculation.py
+++ IHL_calculation.py
@@ -4,6 +4,8 @@ import time
 from multiprocessing import Pool
 from scipy import interpolate
 import math
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 
 def coriolis_twist(u,v):
@@ -14,8 +16,26 @@ def coriolis_twist(u,v):
 
 def Horizontal_velocity(it):
     f = interpolate.interp1d(h,twist)
-    f_ux = interpolate.interp1d(h,ux_mean_profile)
     mag_horz_vel = []
+    for i in np.arange(0,len(zs)):
+        u_i = u[it,i*x:(i+1)*x]; v_i = v[it,i*x:(i+1)*x]
+        if zs[i] < h[0]:
+            twist_h = f(h[0])
+
+        elif zs[i] > h[-1]:
+            twist_h = f(h[-1])
+        else:
+            twist_h = f(zs[i])
+
+        mag_horz_vel_i = u_i*np.cos(twist_h) + v_i*np.sin(twist_h)
+        mag_horz_vel.extend(mag_horz_vel_i)
+    mag_horz_vel = np.array(mag_horz_vel)
+    return mag_horz_vel
+
+
+def Fluc_Horizontal_velocity(it):
+    f = interpolate.interp1d(h,twist)
+    f_ux = interpolate.interp1d(h,ux_mean_profile)
     mag_fluc_horz_vel = []
     for i in np.arange(0,len(zs)):
         u_i = u[it,i*x:(i+1)*x]; v_i = v[it,i*x:(i+1)*x]
@@ -32,17 +52,27 @@ def Horizontal_velocity(it):
 
         mag_horz_vel_i = u_i*np.cos(twist_h) + v_i*np.sin(twist_h)
         mag_fluc_horz_vel_i = np.subtract(mag_horz_vel_i,ux_mean)
-        mag_horz_vel.extend(mag_horz_vel_i)
         mag_fluc_horz_vel.extend(mag_fluc_horz_vel_i)
-    mag_horz_vel = np.array(mag_horz_vel)
     mag_fluc_horz_vel = np.array(mag_fluc_horz_vel)
-    return mag_horz_vel, mag_fluc_horz_vel
+    return mag_fluc_horz_vel
 
 
 def Update(it):
 
     U = u[it]
     U_pri = u_pri[it]
+
+    u_plane = U.reshape(y,x)
+    Xs,Ys = np.meshgrid(ys,zs)
+
+    Z = u_plane
+
+    fig,ax = plt.subplots(figsize=(50,30))
+    plt.rcParams['font.size'] = 40
+
+    cs = ax.contourf(Xs,Ys,Z,levels=levels, cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
+
+    cb = plt.colorbar(cs)
 
     AH = 0; AL = 0; AI = 0
     IyH = 0; IyL = 0; IyI = 0; Iy = 0
@@ -65,16 +95,19 @@ def Update(it):
                     IyH+=(Uijk*k*dA)
                     IzH+=(Uijk*j*dA)
                     UxH.append(Uijk)
+                    plt.plot(ys[ijk],zs[ijk],"ok")
                 elif U_pri_ijk <= -0.7:
                     AL+=dA
                     IyL+=(Uijk*k*dA)
                     IzL+=(Uijk*j*dA)
                     UxL.append(Uijk)
+                    plt.plot(ys[ijk],zs[ijk],"+k")
                 else:
                     AI+=dA
                     IyI+=(Uijk*k*dA)
                     IzI+=(Uijk*j*dA)
                     UxI.append(Uijk)
+                    plt.plot(ys[ijk],zs[ijk],"sk")
         ijk+=1
 
     if len(UxH) > 0:
@@ -91,6 +124,30 @@ def Update(it):
         UxI = np.average(UxI)
     else:
         UxI = 0
+
+
+    plt.xlabel("y' axis (rotor frame of reference) [m]",fontsize=40)
+    plt.ylabel("z' axis (rotor frame of reference) [m]",fontsize=40)
+    plt.xticks(fontsize=40)
+    plt.yticks(fontsize=40)
+
+
+    #define titles and filenames for movie
+    Title = "Rotor Plane. \nFluctuating horizontal velocity [m/s]: Offset = -63.0m, Time = {}[s]".format(it)
+    filename = "Rotor_Fluc_Horz_-63.0_{}.png".format(it)
+    
+    x_c = [-1,-10]; y_c = [-1,-10]
+    plt.plot(x_c,y_c,"-k",linewidth=5,label="$u_x' \geq 0.7 m/s$")
+    plt.plot(x_c,y_c,"--k",linewidth=5,label="$u_x' \leq -0.7 m/s$")
+
+    plt.xlim([ys[0],ys[-1]]);plt.ylim(zs[0],zs[-1])
+    plt.legend()
+    plt.title(Title)
+    plt.tight_layout()
+    plt.savefig(in_dir+filename)
+    plt.cla()
+    cb.remove()
+    plt.close(fig)
 
     return AH,AL,AI,IyH,IyL,IyI,IzH,IzL,IzI,Iy,Iz,UxH,UxL,UxI
 
@@ -180,15 +237,27 @@ del p
 
 u[u<0]=0; v[v<0] #remove negative velocities
 
+#fluctuating streamwise velocity
 with Pool() as pool:
-    u_hvel = []; u_pri = []
-    for u_hvel_it, u_fluc_hvel_it in pool.imap(Horizontal_velocity,Time_steps):
+    u_pri = []
+    for u_fluc_hvel_it in pool.imap(Fluc_Horizontal_velocity,Time_steps):
+        
+        u_pri.append(u_fluc_hvel_it)
+        print(len(u_pri),time.time()-start_time)
+u_pri = np.array(u_pri)
+
+cmin = math.floor(np.min(u_pri))
+cmax = math.ceil(np.max(u_pri))
+print("line 184",cmin,cmax)
+
+#streamwise velocity
+with Pool() as pool:
+    u_hvel = []
+    for u_hvel_it in pool.imap(Horizontal_velocity,Time_steps):
         
         u_hvel.append(u_hvel_it)
-        u_pri.append(u_fluc_hvel_it)
         print(len(u_hvel),time.time()-start_time)
-u = np.array(u_hvel); del u_hvel; del v
-u_pri = np.array(u_pri)
+u = np.array(u_hvel); del u_pri; del v
 
 print("line 139",time.time()-start_time)
 
@@ -196,25 +265,50 @@ cmin = math.floor(np.min(u))
 cmax = math.ceil(np.max(u))
 print("line 184",cmin,cmax)
 
+nlevs = int((cmax-cmin)/2)
+if nlevs>abs(cmin) or nlevs>cmax:
+    nlevs = min([abs(cmin),cmax])+1
+
+levs_min = np.linspace(cmin,0,nlevs,dtype=int); levs_max = np.linspace(0,cmax,nlevs,dtype=int)
+levels = np.concatenate((levs_min,levs_max[1:]))
+print("line 153", levels)
+
+time.sleep(5)
+
 it = 0
 A_High_arr = []; A_Low_arr = []; A_Int_arr = []
 Iy_High_arr = []; Iy_Low_arr = []; Iy_Int_arr = []
 Iz_High_arr = []; Iz_Low_arr = []; Iz_Int_arr = []
 Ux_High_arr = []; Ux_Low_arr = []; Ux_Int_arr = []
 Iy_arr = []; Iz_arr = []
-with Pool() as pool:
-    for AH,AL,AI,IyH,IyL,IyI,IzH,IzL,IzI,Iy_it,Iz_it,UxH_it,UxL_it,UxI_it in pool.imap(Update,Time_steps):        
+#with Pool() as pool:
+    #for AH,AL,AI,IyH,IyL,IyI,IzH,IzL,IzI,Iy_it,Iz_it,UxH_it,UxL_it,UxI_it in pool.imap(Update,Time_steps):        
     
-        print("time step",it)
+print("time step",it)
+AH,AL,AI,IyH,IyL,IyI,IzH,IzL,IzI,Iy_it,Iz_it,UxH_it,UxL_it,UxI_it = Update(it)
+A_High_arr.append(AH); A_Low_arr.append(AL); A_Int_arr.append(AI)
+Iy_High_arr.append(IyH); Iy_Low_arr.append(IyL); Iy_Int_arr.append(IyI)
+Iz_High_arr.append(IzH); Iz_Low_arr.append(IzL); Iz_Int_arr.append(IzI)
+Ux_High_arr.append(UxH_it); Ux_Low_arr.append(UxL_it); Ux_Int_arr.append(UxI_it)
+Iy_arr.append(Iy_it); Iz_arr.append(Iz_it)
 
-        A_High_arr.append(AH); A_Low_arr.append(AL); A_Int_arr.append(AI)
-        Iy_High_arr.append(IyH); Iy_Low_arr.append(IyL); Iy_Int_arr.append(IyI)
-        Iz_High_arr.append(IzH); Iz_Low_arr.append(IzL); Iz_Int_arr.append(IzI)
-        Ux_High_arr.append(UxH_it); Ux_Low_arr.append(UxL_it); Ux_Int_arr.append(UxI_it)
-        Iy_arr.append(Iy_it); Iz_arr.append(Iz_it)
+print(AH)
+print(AL)
+print(AI)
+print(IyH)
+print(IyL)
+print(IyI)
+print(IzH)
+print(IzL)
+print(IzI)
+print(Iy_it)
+print(Iz_it)
+print(UxH_it)
+print(UxL_it)
+print(UxI_it)
 
-        print("line 188",time.time()-start_time)
-        it+=1
+print("line 188",time.time()-start_time)
+it+=1
 
 
 ncfile = Dataset(out_dir+"Asymmetry_Dataset.nc",mode="w",format='NETCDF4')
