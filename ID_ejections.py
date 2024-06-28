5b78253092c2f180d786ae5e0e1d4504b4283070
JarredK21 <jarred.kenworthy@strath.ac.uk>
2024-06-28 15:12:37 +0100

    debug ID_ej 2

--- ID_ejections.py
+++ ID_ejections.py
@@ -40,16 +40,6 @@ def Horizontal_velocity(it):
     mag_fluc_horz_vel = np.array(mag_fluc_horz_vel)
     return mag_fluc_horz_vel
 
-
-def isInside(x, y):
-     
-    if ((x - 2560) * (x - 2560) +
-        (y - 90) * (y - 90) < 63 * 63):
-        return True
-    else:
-        return False
-    
-
 start_time = time.time()
 
 #defining twist angles with height from precursor
@@ -163,77 +153,6 @@ if isExist == False:
     os.makedirs(folder)
 
 
-#options
-plot_thresholds = False
-output_data = False
-output_heights = True
-
-
-def Update(it):
-
-    U_pri = u_pri[it] #velocity time step it
-    
-    if it < 10:
-        Time_idx = "000{}".format(it)
-    elif it >= 10 and it < 100:
-        Time_idx = "00{}".format(it)
-    elif it >= 100 and it < 1000:
-        Time_idx = "0{}".format(it)
-    elif it >= 1000 and it < 10000:
-        Time_idx = "{}".format(it)
-
-
-
-    Z = U_pri.reshape(y,x)
-    X,Y = np.meshgrid(ys,zs)
-
-    T = Time[it]
-
-    fig,ax = plt.subplots(figsize=(50,30))
-    plt.rcParams['font.size'] = 40
-
-    cs = ax.contourf(X,Y,Z,levels=levels, cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
-
-    cb = plt.colorbar(cs)
-
-    Drawing_uncolored_circle = Circle( (2560, 90),radius=63 ,fill = False, linewidth=1)
-    ax.add_artist(Drawing_uncolored_circle)
-
-
-    for t in np.arange(0,len(thresholds)):
-        storage = np.zeros(len(ys))
-        for j in np.arange(0,len(ys)):
-            for k in np.arange(0,len(zs)-1):
-
-                if Z[k+1,j] > thresholds[t]:
-                    storage[j] = zs[k]
-                    break
-
-        ax.plot(ys,storage,linewidth=2,label="{}m/s".format(thresholds[t]))
-
-
-    plt.xlabel("y' axis (rotor frame of reference) [m]",fontsize=40)
-    plt.ylabel("z' axis (rotor frame of reference) [m]",fontsize=40)
-    plt.xticks(fontsize=40)
-    plt.yticks(fontsize=40)
-    ax.legend(loc="upper right")
-
-
-    #define titles and filenames for movie
-    Title = "Rotor Plane. \nFluctuating horizontal velocity [m/s]: Offset = -63.0m, Time = {0}[s]".format(round(T,4))
-    filename = "Rotor_Fluc_Horz_-63.0_{0}.png".format(Time_idx)
-        
-
-    plt.title(Title)
-    plt.tight_layout()
-    plt.savefig(folder+filename)
-    plt.cla()
-    cb.remove()
-    plt.close(fig)
-
-    return T
-
-
 def Update_locs(it):
 
     #algorithm for ejections
@@ -253,157 +172,47 @@ def Update_locs(it):
     return H
 
 
-def Update_data(it):
-
-    #algorithm for ejections
-
-    U_pri = u_pri[it] #velocity time step it
-
-    u_plane = U_pri.reshape(y,x)
-
-    H = []
-    jt = 0
-    for j in np.arange(0,len(ys)):
-        for k in np.arange(0,len(zs)-1):
-
-            if u_plane[k+1,j] > threshold:
-
-                break
-        
-        #if coordinate over lowest point of rotor disk regardles of y' location
-        if zs[k] >= 27:
-            jt+=1
-
-        #is coordinate inside rotor disk
-        cc = isInside(ys[j],zs[k])
-        if cc == True:
-            z = np.min( np.roots([1,-180,(90**2-63**2+(ys[j]-2560)**2)]) )
-            H.append(zs[k]-z) #height from coordinate zs to coordinate z on rotor disk
-
-        #is coordinate above rotor disk so it is still covering it
-        elif ys[j] > 2497 and ys[j] < 2623 and zs[k] > 90:
-            z = np.roots([1,-180,(90**2-63**2+(ys[j]-2560)**2)])
-            H.append(z[0]-z[1]) #height
 
 
-    #average height
-    if len(H) > 0:
-        H_avg = np.average(H)
-    else:
-        H_avg = 0
+ncfile = Dataset(in_dir+"Threshold_heights_Dataset.nc",mode="w",format="NETCDF4")
+ncfile.title = "Heights at threshold data sampling output"
 
-    #integrate over area covering rotor disk
-    A = 0
-    for i in np.arange(0,len(H)-1):
-        A+=((H[i+1] + H[i])/2)*dy
+#create global dimensions
+sampling_dim = ncfile.createDimension("sampling",None)
+y_dim = ncfile.createDimension("num_points",None)
+
+Time_sampling = ncfile.createVariable("Time", np.float64, ('sampling',),zlib=True)
+Time_sampling[:] = Time
+y_locs = ncfile.createVariable("ys", np.float64, ('num_points',),zlib=True)
+y_locs[:] = ys
+
+#thresholds to output data
+thresholds = [-5.0,-4.5,-4.0,-3.5,-3.0,-2.5,-2.0,-1.4]
 
 
-    return A, H_avg, jt
+for threshold in thresholds:
 
+    print("line 293",threshold)
 
-if plot_thresholds == True:
-    #thresholds to plot
-    thresholds = [-1.4,-2.5,-5.0]
+    group = ncfile.createGroup("{}".format(abs(threshold)))
 
+    H_ejection = group.createVariable("Height_ejection", np.float64, ('sampling','num_points'),zlib=True)
+
+    H_array = []
+    ix = 1
     with Pool() as pool:
-        for T in pool.imap(Update,Time_steps):
+        for H_it in pool.imap(Update_locs,Time_steps):
 
-            print(T,time.time()-start_time)
+            H_array.append(H_it)
 
+            print(ix,time.time()-start_time)
 
+            ix+=1
 
-if output_heights == True:
-    ncfile = Dataset(in_dir+"Threshold_heights_Dataset.nc",mode="w",format="NETCDF4")
-    ncfile.title = "Heights at threshold data sampling output"
+    H_ejection[:] = np.array(H_array); del H_array
 
-    #create global dimensions
-    sampling_dim = ncfile.createDimension("sampling",None)
-    y_dim = ncfile.createDimension("num_points",None)
-
-    Time_sampling = ncfile.createVariable("Time", np.float64, ('sampling',),zlib=True)
-    Time_sampling[:] = Time
-    y_locs = ncfile.createVariable("ys", np.float64, ('sampling',),zlib=True)
-    y_locs[:] = ys
-
-    #thresholds to output data
-    thresholds = [-5.0,-4.5,-4.0,-3.5,-3.0,-2.5,-2.0,-1.4]
-
-    
-    for threshold in thresholds:
-
-        print("line 293",threshold)
-
-        group = ncfile.createGroup("{}".format(abs(threshold)))
-
-        H_ejection = group.createVariable("Height_ejection", np.float64, ('sampling','num_points'),zlib=True)
-
-        H_array = []
-        ix = 1
-        with Pool() as pool:
-            for H_it in pool.imap(Update_locs,Time_steps):
-
-                H_array.append(H_it)
-
-                print(ix,time.time()-start_time)
-
-                ix+=1
-
-        H_ejection[:] = np.array(H_array); del H_array
-
-        print(ncfile.groups)
+    print(ncfile.groups)
 
 
-    print(ncfile)
-    ncfile.close()
-
-
-
-if output_data == True:
-    #create netcdf file
-    ncfile = Dataset(in_dir+"Thresholding_Dataset.nc",mode="w",format='NETCDF4')
-    ncfile.title = "Threshold data sampling output"
-
-    #create global dimensions
-    sampling_dim = ncfile.createDimension("sampling",None)
-
-    Time_sampling = ncfile.createVariable("Time", np.float64, ('sampling',),zlib=True)
-    Time_sampling[:] = Time
-
-    #thresholds to output data
-    thresholds = np.linspace(cmin,-0.7,8)
-
-    
-    for threshold in thresholds:
-
-        print("line 293",threshold)
-
-        group = ncfile.createGroup("{}".format(round(abs(threshold),1)))
-
-        A_ejection = group.createVariable("Area_ejection", np.float64, ('sampling'),zlib=True)
-        H_average = group.createVariable("Average_height", np.float64, ('sampling'),zlib=True)
-        jt_ejections = group.createVariable("times_ejection", np.float64, ('sampling'),zlib=True)
-
-        A_ej = []
-        H_avg_array = []
-        jt_array = []
-        ix = 1
-        with Pool() as pool:
-            for A_adv_it, H_avg_it,jt_it in pool.imap(Update_data,Time_steps):
-
-                A_ej.append(A_adv_it)
-                H_avg_array.append(H_avg_it)
-                jt_array.append(jt_it)
-
-                print(ix,time.time()-start_time)
-
-                ix+=1
-
-        A_ejection[:] = np.array(A_ej); del A_ej
-        H_average[:] = np.array(H_avg_array); del H_avg_array
-        jt_ejections[:] = np.array(jt_array); del jt_array
-
-        print(ncfile.groups)
-
-
-    print(ncfile)
-    ncfile.close()
+print(ncfile)
+ncfile.close()
--- test.py
+++ test.py
@@ -1,6 +1,32 @@
 import numpy as np
+from netCDF4 import Dataset
 
-levels_neg = np.linspace(-5.0,-1.4,8)
-print("line 159", levels_neg)
+in_dir = "../../NREL_5MW_MCBL_R_CRPM_3/post_processing/"
+a = Dataset(in_dir+"sampling_r_-63.0_0.nc")
 
-[-5.0,-4.5,-4.0,-3.5,-3.0,-2.5,-2.0,-1.4]
+#rotor data
+p = a.groups["p_r"]; del a
+
+x = p.ijk_dims[0] #no. data points
+y = p.ijk_dims[1] #no. data points
+
+normal = 29
+
+#define plotting axes
+coordinates = np.array(p.variables["coordinates"])
+
+xo = coordinates[0:x,0]
+yo = coordinates[0:x,1]
+
+rotor_coordiates = [2560,2560,90]
+
+x_trans = xo - rotor_coordiates[0]
+y_trans = yo - rotor_coordiates[1]
+
+phi = np.radians(-normal)
+xs = np.subtract(x_trans*np.cos(phi), y_trans*np.sin(phi))
+ys = np.add(y_trans*np.cos(phi), x_trans*np.sin(phi))
+xs = xs + rotor_coordiates[0]
+ys = ys + rotor_coordiates[1]
+
+test = "end"
